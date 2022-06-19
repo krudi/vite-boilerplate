fab203d822a789a2e9576ca6ac93df0eff0a8e2c
Patryk Kudlik <patryk@web-vision.de>
2022-06-19 18:16:36 +0200

    fix: Updating linting files command

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,14 @@
 import { defineConfig } from 'vite'
+import path from 'path'
 
 export default defineConfig({
 	css: {
 		preprocessorOptions: {
 			scss: {
 				additionalData: `
-          @import "./assets/scss/abstracts/colors.scss";
-          @import "./assets/scss/abstracts/fonts.scss";
-        `
+                    @import "./assets/scss/abstracts/colors.scss";
+                    @import "./assets/scss/abstracts/fonts.scss";
+                `
 			}
 		}
 	}
