c1d099552bdb9b3be4e5bb17e05f1048f6b30c6d
Patryk Kudlik <patryk@web-vision.de>
2022-06-25 22:55:56 +0200

    feat: Removing pre-commit tests

--- husky.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-	hooks: {
-		'pre-commit': 'yarn lint:js && lint:css && lint:files',
-		'pre-push': 'yarn lint-fix && yarn test'
-	}
-}
--- lint-staged.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	'lint-staged': {
-		'*.{ts,js}': ['npx eslint --ext .js,.vue .', 'git add'],
-		'*.{css,scss}': ['npx stylelint .css,.scss .', 'git add'],
-		'*.{json,txt}': ['npx prettier --write .', 'git add']
-	}
-}
